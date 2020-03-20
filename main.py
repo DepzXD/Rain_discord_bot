13a3d4d63d4ea1883d063f5185b717a22c240084
Deepak <dk721572@gmail.com>
2020-03-20 13:51:52 +0530

    Rain's now send message when user joins or leaves

--- main.py
+++ main.py
@@ -5,13 +5,40 @@ import os
 load_dotenv()
 
 token = os.getenv('TOKEN')
+client = discord.Client()
 
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print('Logged on as {0}!'.format(self.user))
+@client.event
+async def on_ready():
+  print('Rain is ready for battle!!')
 
-    async def on_message(self, message):
-        print('Message from {0.author}: {0.content}'.format(message))
+# welcomes new users 
+@client.event
+async def on_member_join(member):
+  for channel in member.guild.channels:
+    if str(channel) == "the-lobby":
+      embed = discord.Embed(title=f"{member}", color=0x2a8ef8)
+      embed.set_thumbnail(url=member.avatar_url)
+      embed.add_field(name="!hello", value=f"""{member.mention} has joined the battle bus.""")
+      await channel.send(content=None, embed=embed)
 
-client = MyClient()
+# user lefts 
+@client.event
+async def on_member_remove(member):
+  for channel in member.guild.channels:
+    if str(channel) == "the-lobby":
+      embed = discord.Embed(title=f"{member}", color=0xe9ec36)
+      embed.set_thumbnail(url=member.avatar_url)
+      embed.add_field(name="yo", value=f"""{member.mention} has lefts the battle bus.""")
+      await channel.send(content=None, embed=embed)
+
+# chat sec  
+@client.event
+async def on_message(message):
+  id = client.get_guild(os.getenv('SERVER_ID'))
+  channels = ["rain-discord-bot"]
+  if str(message.channel) in channels:
+    if message.content.find("hello") != -1:
+      # Todo: add name split id of author 
+      await message.channel.send(f"👋 Hi {message.author}") 
+
 client.run(token)
